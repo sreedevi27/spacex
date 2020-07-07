5fbb390ffeabdd12663659472f64929d69f61f8c
sree_devi27@yahoo.com <sreedeviramanadham@Sreedevis-Air.satx.rr.com>
2020-07-06 21:36:49 -0500

    Added header component andstyle.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
+import Header from "./components/Header/Header.js";
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome .....</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./style.css";
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Welcome to space-x application...</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
