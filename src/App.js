75f999e644857067fc229d2c4b6a2a597ba45025
sree_devi27@yahoo.com <sreedeviramanadham@Sreedevis-Air.satx.rr.com>
2020-07-06 20:52:24 -0500

    Changed wecome text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Welcome .....</div>;
 }
 
 export default App;
