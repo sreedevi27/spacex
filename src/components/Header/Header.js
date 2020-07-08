024fa7021e997c09c857494048cf714207a3b7d3
sree_devi27@yahoo.com <sreedeviramanadham@Sreedevis-Air.satx.rr.com>
2020-07-08 15:34:42 -0500

    some changes to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h1>Welcome to space-x application...</h1>
+        <h1>Welcome to awesome space-x application...</h1>
       </div>
     );
   }
